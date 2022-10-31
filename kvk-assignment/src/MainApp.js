bfe5521e6d7bdf57eca5cc852961de2af2cc7158
Peter van der Meer <plvdmeer@gmail.com>
2022-10-31 15:40:12 +0100

    added fetch to get the API data
    could probably change to Redux for a more persistent app state

--- kvk-assignment/src/MainApp.js
+++ kvk-assignment/src/MainApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {handleChange} from './functions.js';
+import {handleChange, apiGET} from './functions.js';
 
 import './App.css';
 
@@ -9,18 +9,27 @@ class MainApp extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {search_form_value:""};
+    this.state = {search_form_value:"", search_results:{}};
   }
 
   render() {
     return (
       <div className="container">
-        <form onSubmit={(e) => {e.preventDefault();this.refresh()}}>
+        <form onSubmit={(e) => {e.preventDefault();this.getData()}}>
           <div className="form-field col-2"><input type="text" value={this.state.search_form_value} onChange={(e) => handleChange(this, "search_form_value", e)} placeholder="zoek hier.." /></div>
           <div className="form-actions col-2"><input type="submit" value="zoeken" /></div>
         </form>
       </div>
     );
+  }
+
+  getData() {
+    apiGET("https://617c09aad842cf001711c200.mockapi.io/v1/companies", "getData").then(function(_response) {
+      console.log(_response)
+      handleChange(this, "search_results", _response);
+    }.bind(this)).catch((err) => {
+      console.error("getData error"+err);
+    });
   }
 
 }
--- kvk-assignment/src/functions.js
+++ kvk-assignment/src/functions.js
@@ -1,6 +1,34 @@
+var fetch = require('isomorphic-fetch');
 
+
+/**
+* handle state change in context's state
+*/
 export function handleChange(context, state_name, e) {
   var newState = {};
   newState[state_name] = (typeof e.target !== "undefined") ? e.target.value : e;
   context.setState(newState);
+}
+
+/**
+* generic API GET Request
+*/
+export function apiGET(api_url, error_id) {
+  return new Promise(function(resolve, reject) {
+    fetch(
+      api_url,
+      {}
+    )
+    .then(function(response) {
+        resolve(response.json());
+      },
+      function(err) {
+        // Error WHILE fetching
+        let err_key = "apiGET_error_while";
+        console.error("["+new Date()+"]["+err_key+"] "+api_url);
+        reject(err_key);
+        // this is also thrown if the database or the API is offline/unreachable
+      }
+    );;
+  });
 }
