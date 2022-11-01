54b10a11bf6b9267d5f46afb3de6473f2d1fc9ee
Peter van der Meer <plvdmeer@gmail.com>
2022-11-01 13:49:12 +0100

    more data for search results
    and some styling

--- kvk-assignment/src/SearchResults.js
+++ kvk-assignment/src/SearchResults.js
@@ -8,11 +8,11 @@ class SearchResults extends React.Component {
       let display_data = this.props.search_results.data;
       let return_results = [];
       for(let i in display_data) {
-        return_results.push(<div key={i}>{display_data[i].name}</div>);
+        return_results.push(<div key={i}><h3>{display_data[i].name}</h3><p>Plaats: {display_data[i].city}</p><p>Straat: {display_data[i].streetName}</p><p>Postcode: {display_data[i].zipCode}</p></div>);
       }
 
       return (
-        <div>{return_results}</div>
+        <div id="search-results">{return_results}</div>
       );
     } else {
       return null;
